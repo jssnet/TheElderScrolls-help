2f508d29f51163186ce961aa192b76e12ca1751e
the-construction-worker <22092182+the-construction-worker@users.noreply.github.com>
2018-03-27 22:27:20 -0400

    Initial commit

---
+++ morrowind oblivion skyrim challenge curve.py
@@ -0,0 +1,22 @@
+'''
+Created on 2017-05-11 May 11th, 2017
+worked on 2017-05-11
+'''
+# <-- compare, The Elder Scrolls:  Online, World of Warcraft
+
+#with Morrowind EOS trailer, "I know how to play gaimz!  Play gaimz!  He even has Athene helping him!"
+'''
+long/ short term advantages example quests/ caves "really hard monsters"
+-->  "really hard monsters" example cave @ level with treasure, monsters "new monsters now!"
+-->
+--> #gudwon
+
+
+'''
+'''
+game starts: [your character sucks]
+example:  Block 5 vs mudcrab
+[Armour 11] 90HP
+1 - 5
+mudcrab not dead
+'''
---
+++ skyrim cat stats.py
@@ -0,0 +1,5 @@
+'''
+Created on 2017-05-04 May 4th, 2017
+worked on 2017-05-04
+'''
+# python code reuse starts with import statements from the top of the page.
